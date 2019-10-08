e74f13830dc7924ffd1b4c3a9a03c20c6a885323
Jake Hertz <hertzjake@gmail.com>
2019-10-07 20:57:38 -0400

    we did it!

---
+++ scripts.js
@@ -0,0 +1,118 @@
+const generateNxN = (n)=> {
+    const elements = 'red blue green yellow aqua'.split(' ');
+    const arr =[];
+    let position = 0;
+    for (let i = 0; i < n; i++) {
+        const innerArr = [];
+        for (let j = 0; j < n; j++) {
+            const el = elements[position];
+            innerArr.push(el);
+            elements.shift();
+            elements.push(el);
+        }
+        arr.push(innerArr)
+    }
+    return arr;
+};
+
+const originalGrid = generateNxN(5);
+
+const colors = [...document.querySelectorAll('.color')];
+
+
+
+let currentColor = 'white';
+
+colors.forEach(color => {
+    color.addEventListener("click", ev => {
+        if (currentColor === 'white') {
+            ev.target.classList.toggle('selected')
+            currentColor = ev.target.getAttribute('id');
+        }
+        else {
+            let currentSelected = document.querySelector(`#${currentColor}`)
+            currentSelected.classList.remove('selected');
+            ev.target.classList.toggle('selected')
+            currentColor = ev.target.getAttribute('id');
+        }  
+    })
+})
+
+
+
+const generateHTMLGrid = (arr)=> { 
+    const canvas = document.querySelector('.grid');
+    arr.forEach(row => {
+        const rowDiv = document.createElement('div');
+        rowDiv.classList.add('row')
+        row.forEach(box => {
+            const cell = document.createElement('div');
+            cell.classList.add('cell')
+            rowDiv.appendChild(cell);
+            cell.addEventListener('click', ev => {
+                cell.style.backgroundColor = currentColor;
+            })
+        })
+        canvas.appendChild(rowDiv);
+    })
+
+};
+
+let countRows = 5;
+let countCol = 5;
+
+const addRowButton = document.querySelector('#addRow');
+const removeRowButton = document.querySelector('#removeRow');
+const addColButton = document.querySelector('#addCol');
+const removeColButton = document.querySelector('#removeCol')
+
+generateHTMLGrid(originalGrid);
+
+let rows = [...document.querySelectorAll('.row')]
+
+addRowButton.addEventListener('click', ev => {
+    const canvas = document.querySelector('.grid');
+    const rowDiv = document.createElement('div');
+    rowDiv.classList.add('row');
+    countRows++;
+    for (let i = 0; i < countCol; i++) {
+        const cell = document.createElement('div');
+        cell.classList.add('cell')
+        rowDiv.appendChild(cell);
+        cell.addEventListener('click', ev => {
+            cell.style.backgroundColor = currentColor;
+        })
+    }
+    canvas.appendChild(rowDiv);
+    rows = [...document.querySelectorAll('.row')]
+})
+
+removeRowButton.addEventListener('click', ev => {
+    countRows--;
+    const canvas = document.querySelector('.grid');
+    const lastRow = document.querySelector('.row:last-child');
+    canvas.removeChild(lastRow);
+    rows = [...document.querySelectorAll('.row')]
+})
+
+addColButton.addEventListener('click', ev => {
+    rows.forEach(row => {
+        const cell = document.createElement('div');
+        cell.classList.add('cell')
+        row.appendChild(cell);
+        cell.addEventListener('click', ev => {
+            cell.style.backgroundColor = currentColor;
+        })
+    })
+    countCol++
+})
+
+removeColButton.addEventListener('click', ev => {
+    countCol--;
+    const lastCells = [...document.querySelectorAll('.cell:last-child')];
+    for (let i = 0; i < rows.length; i++) {
+        const row = rows[i];
+        const cell = lastCells[i];
+        row.removeChild(cell);
+    }
+})
